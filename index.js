97a561120b1ac0ea2b6972f8c87c323e7be4b2be
yaxpaccruzjuarez <ypjuarez11@gmail.com>
2022-04-27 12:04:39 -0400

    commit tree 836e2ef8dd2b1b102035e08cb42488fa519d77f6

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
-}
+  if (ride < 400){
+    return 'This one is on me!'
+} else if (ride > 400 && ride < 2000) {
+  return 'That will be twenty bucks.'
+} else if (ride > 2000 && ride < 2499 ){
+  return 'I will gladly take your thirty bucks.'
+}else if (ride >= 2500){ 
+    return 'No can do.'
+}}
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
-}
+
+  return (city === 'NYC' ? 'Ok, sounds good.' : 'No go.')
+    
+} 
 
 function switchOnCharmFromTip(){
   // Write your code here!
+  const tip = 'generous'
+
+
+  switch (tip) {
+    case 'generous' :  "Thank you so much.";
+      
+      break;
+
+     
+  }
+  return tip
+
 }
